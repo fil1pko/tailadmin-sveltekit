d2a1559799f68ba2e6012b5023de1a17484eb287
fil1pko <fsustek60@gmail.com>
2026-04-14 14:55:09 +0200

    feat(video): migrate video components to sveltekit with full dx fixes

---
+++ src/components/videos/OneIsToOne.svelte
@@ -0,0 +1,9 @@
+<div class="aspect-square overflow-hidden rounded-lg">
+     <iframe
+          src="https://www.youtube.com/embed/dQw4w9WgXcQ"
+          title="YouTube video"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          allowfullscreen
+          class="h-full w-full border-0"
+     ></iframe>
+</div>
