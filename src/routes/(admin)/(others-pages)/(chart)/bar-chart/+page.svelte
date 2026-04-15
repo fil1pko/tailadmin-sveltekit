33549becccc413eb0e55d14103c72b639bd93c88
fil1pko <fsustek60@gmail.com>
2026-04-15 15:47:51 +0200

    feat(charts): migrate all charts pages to sveltekit with full dx fixes

---
+++ src/routes/(admin)/(others-pages)/(chart)/bar-chart/+page.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+     import BarChartOne from "$components/charts/bar/BarChartOne.svelte";
+     import ComponentCard from "$components/common/ComponentCard.svelte";
+     import PageBreadcrumb from "$components/common/PageBreadCrumb.svelte";
+</script>
+
+<svelte:head>
+     <title>Svelte Bar Chart | TailAdmin - Svelte Dashboard Template</title>
+     <meta
+          name="description"
+          content="This is Svelte Bar Chart page for TailAdmin - Svelte Tailwind CSS Admin Dashboard Template"
+     />
+</svelte:head>
+
+<div>
+     <PageBreadcrumb pageTitle="Bar Chart" />
+
+     <div class="space-y-6">
+          <ComponentCard title="Bar Chart 1">
+               <BarChartOne />
+          </ComponentCard>
+     </div>
+</div>
---
+++ src/routes/(admin)/(others-pages)/(chart)/line-chart/+page.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+     import LineChartOne from "$components/charts/line/LineChartOne.svelte";
+     import ComponentCard from "$components/common/ComponentCard.svelte";
+     import PageBreadcrumb from "$components/common/PageBreadCrumb.svelte";
+</script>
+
+<svelte:head>
+     <title>Svelte Line Chart | TailAdmin - Svelte Dashboard Template</title>
+     <meta
+          name="description"
+          content="This is Svelte Line Chart page for TailAdmin - Svelte Tailwind CSS Admin Dashboard Template"
+     />
+</svelte:head>
+
+<div>
+     <PageBreadcrumb pageTitle="Line Chart" />
+
+     <div class="space-y-6">
+          <ComponentCard title="Line Chart 1">
+               <LineChartOne />
+          </ComponentCard>
+     </div>
+</div>
